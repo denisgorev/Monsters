f41729649b933a7b0f7baada4d8b4242a1a494c2
Gorev <denis.gorev@accenture.com>
2020-09-10 10:41:09 +0300

    Updates

---
+++ precache-manifest.904bdfa0aea596ef600c6555ffba358f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58080515495baa759451ae21c48a4cba",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "ebbf1bedfd0bd0c55fea",
+    "url": "/Monsters/static/css/main.dc31c093.chunk.css"
+  },
+  {
+    "revision": "57595fc6caf45f548129",
+    "url": "/Monsters/static/js/2.c272e29f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters/static/js/2.c272e29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebbf1bedfd0bd0c55fea",
+    "url": "/Monsters/static/js/main.cac46ea4.chunk.js"
+  },
+  {
+    "revision": "99d8a27463727051162c",
+    "url": "/Monsters/static/js/runtime-main.3abc2b3c.js"
+  }
+]);
